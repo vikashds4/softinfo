e4025ea4452820bbb3ae8eb0bae91e57403261f9
Vikash Kumar Dubey <vikiashdubey2552@gmail.com>
2021-06-17 21:16:22 +0530

    softinfo email id

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ function sendmail(){
 
        Email.send({
    SecureToken:"fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
-           To: 'anjani.pathak@zensark.com',
+           To: 'info@softinfodynamics.com',
            From: "demooptimalweb@gmail.com",
            Subject: "New message on contact from "+name,
            Body: Body
